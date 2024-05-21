2facaa83c838a7d2a3b073d67a132807b4af4754
Jason Tran <jtran64@wgu.edu>
2024-05-21 11:52:15 -0700

    Added logging feature

--- main.py
+++ main.py
@@ -2,50 +2,95 @@ import pyautogui
 import time
 import webbrowser
 import os
+import re
+import logging
 from pathlib import Path
 
-# Mouse Functions
-# print(pyautogui.size())  # Prints resolution of screen (width=3440, height=1440)
-# print(pyautogui.position())  # Prints position of mouse currently
-# pyautogui.moveTo(100, 100, 4)  # moves mouse to position over time, can remove
-# pyautogui.moveRel(100, 100, 3) # moves relative to current position
-# pyautogui.click(500, 500, 3, 3, button="left")
-
-# pyautogui.scroll(500)
-# Mouse up and down (click and dragging)
-# pyautogui.mouseDown(500, 500, button="left")
-# pyautogui.moveTo(550, 500, 3)
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    filename='logfiles.log',
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+count = 160
 
 
-def download_image(file_name):
+def download_image(name: str, link: str) -> None:
+    # Initial browser open to incorrect page
+    webbrowser.open(link, 0)
+    time.sleep(3)
+    os.system("taskkill /im chrome.exe /f")
+
+    # Actual browser opening
+    webbrowser.open(link, 0)
+    time.sleep(2)
+
+    # Process of downloading image
     pyautogui.moveTo(1720, 720)
     pyautogui.rightClick()
     pyautogui.moveRel(10, 50)
     pyautogui.leftClick()
     time.sleep(1)
-    pyautogui.write(file_name)
+    pyautogui.write(name)
     pyautogui.press('enter')
-    file_path = Path("C:/Users/jason/Downloads", file_name + ".png")
-    time.sleep(1)
-    print(os.path.exists(file_path))
+    file_path = Path("C:/Users/jason/Downloads", name + ".png")
+
+    # Make sure file is downloaded
+    while not os.path.exists(file_path):
+        time.sleep(1)
+
+    # Logging information
+    global count
+    count += 1
+    logger.info(f"{name} - downloaded ({count})")
+
+    # Close webpage
+    os.system("taskkill /im chrome.exe /f")
+    time.sleep(2)
+
+
+def find_imgur_link(content: str) -> str:
+    # Regex pattern to extract the Imgur link
+    imgur_pattern = re.compile(r'https://i\.imgur\.com/[^\s)]+')
+
+    # Find all matches
+    imgur_matches = imgur_pattern.findall(content)
+
+    # Print the first match if it exists
+    if imgur_matches:
+        return imgur_matches[0]
+    else:
+        return "none"
+
+
+def process_file(root, file_name):
+    file_path = os.path.join(root, file_name)
+    with open(file_path, 'r', encoding="utf-8") as file:
+        content = file.read()
+        name = file_name[:-3]
+        link = find_imgur_link(content)
+        logger.info(f"{name} - {link}")
+        if link == "none":
+            return
+        download_image(name, link)
+
+
+def test():
+    # Purpose scan through the directory
+    directory = r"C:\Users\jason\Desktop\download_imgur_project\Person"
+    for root, dirs, files in os.walk(directory):
+        for file_name in files:
+            process_file(root, file_name)
 
 
 def main():
-    # Open webpage
-    webpage = "https://imgur.com/rfiCiqa"
-    webimage = webpage + ".png"
-    webbrowser.open(webimage, 0)
+    directory = r"C:\Users\jason\Desktop\download_imgur_project\Person"
 
-    time.sleep(1)
-
-    # Download image
-    file_name = "hello"
-    download_image(file_name)
-
-    # Close webpage
-    os.system("taskkill /im chrome.exe")
+    for root, dirs, files in os.walk(directory):
+        for file_name in files:
+            process_file(root, file_name)
 
 
 if __name__ == "__main__":
     main()
-    # test_coordinate()
+    # test()
